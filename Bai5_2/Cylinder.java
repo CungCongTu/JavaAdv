cbf92ae75a4066849ecda72b9d0efe481720791b
CungCongTu <tucc.24ce@vku.udn.vn>
2025-02-23 23:21:13 +0700

    Add files via upload

---
+++ Bai5_2/Cylinder.java
@@ -0,0 +1,29 @@
+package Bai5_2;
+
+public class Cylinder
+{
+    private Circle base;
+    private double height;
+
+    public Cylinder() {
+        base = new Circle();
+        height = 1.0;
+    }
+
+    public Cylinder(Circle base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return base.getArea()*height;
+    }
+
+    public double getArea() {
+        return 2*Math.PI*base.getRadius()*height + 2* base.getArea();
+    }
+}
